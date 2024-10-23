844fab4517929e8bc98392cd64b303852e5848a2
ed-d <edbilal916@gmail.com>
2024-10-23 13:59:54 +0000

    actual new

--- app/page.tsx
+++ app/page.tsx
@@ -96,3 +96,36 @@ export default function Home() {
     </main>
   );
 }
+/*    <input placeholder='name' onChange={e => setS_name(e.target.value)}/>
+      <input type='number' placeholder='radius' onChange={e => setS_radius(Number(e.target.value))}/>
+      <input type='number' placeholder='mass' onChange={e => setS_mass(Number(e.target.value))}/>
+      <input type='number' placeholder='x' onChange={e => setS_x(Number(e.target.value))}/>
+      <input type='number' placeholder='y' onChange={e => setS_y(Number(e.target.value))}/>
+      <input type='number' placeholder='vx' onChange={e => setS_vx(Number(e.target.value))}/>
+      <input type='number' placeholder='vy' onChange={e => setS_vy(Number(e.target.value))}/>
+      <input type='number' placeholder='ax' onChange={e => setS_ax(Number(e.target.value))}/>
+      <input type='number' placeholder='ay' onChange={e => setS_ay(Number(e.target.value))}/>
+
+      <button onClick={() => addObject(s_name, s_radius, s_mass, s_x, s_y, s_vx, s_vy, s_ax, s_ay)} >add</button>
+      <button onClick={() => {
+        let     target = objects.find((({name}) => name == s_name));
+
+        updateObject(target.id, s_name, s_radius, s_mass, s_x, s_y, s_vx, s_vy, s_ax, s_ay);
+      }} >update</button>
+      {
+        objects.map((object) => (
+          <div key={object.id} >
+            <p>{object.id}</p>
+            <p>{object.name}</p> 
+            <p>{object.mass}</p>
+            <p>{object.radius}</p>
+            <p>{object.x}</p>
+            <p>{object.y}</p>
+            <p>{object.vx}</p>
+            <p>{object.vy}</p>
+            <p>{object.ax}</p>
+            <p>{object.ay}</p>
+            <button onClick={ () => deleteObjects(object.id) } >delete</button>
+          </div>
+        ))
+      }*/
