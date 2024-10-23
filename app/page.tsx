13e13f85843a9d0f426a3fd71a684f579654716e
CodeSandbox <support@codesandbox.io>
2024-10-23 03:19:39 +0000

    Sandbox snapshot commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import Image from "next/image";
-import {useState, useEffect, AwaitedReactNode, JSXElementConstructor, ReactElement, ReactNode, ReactPortal} from 'react';
+import {useState, useEffect } from 'react';
 
 function Panel (props: { objs: any; }) {
-  const [selected_obj, setSlected_obj] = useState([]);
-  const [local_objs, setLocal_objs] = useState(props.objs);
+  const [selected_obj, setSelected_obj] = useState(0);
+  const [drop_down_droped_down, setDrop_down_droped_down] = useState(false);
+
+  var drop_d_dis = () => {
+    setDrop_down_droped_down(drop_down_droped_down ? false : true);
+  }
 
   return (
     <div id="panel">
@@ -13,12 +17,12 @@ function Panel (props: { objs: any; }) {
         <p>INSPECT - EDIT - DELETE</p>
       </div>
       <div>
-        <div id="drop_down" >obj_1</div>
+        <div id="drop_down" onClick={() => drop_d_dis()} ><p>{props.objs[selected_obj].name}</p></div>
         {
-          local_objs.length > 0 ?
-            <div id="drop_down_content">
+          drop_down_droped_down ?
+            <div id="drop_down_content" >
               {
-                local_objs.map((item: { name: string  }) => (
+                props.objs.map((item: { name: string  }) => (
                   <p>{item.name}</p>
                 ))
               }
@@ -27,14 +31,75 @@ function Panel (props: { objs: any; }) {
         }
       </div>
       <div>
-        <ul></ul>
+
+        <div><p>name</p><p>{props.objs[selected_obj].name}</p></div>
+        <div><p>solar_mass</p><p>{props.objs[selected_obj].mass}</p></div>
+        <div><p>radius</p><p>{props.objs[selected_obj].radius}</p></div>
+        <div><p>x</p><p>{props.objs[selected_obj].x}</p></div>
+        <div><p>y</p><p>{props.objs[selected_obj].y}</p></div>
+        <div><p>y</p><p>{props.objs[selected_obj].z}</p></div>
+        <div><p>z</p><p>{props.objs[selected_obj].z}</p></div>
+        <div><p>vx</p><p>{props.objs[selected_obj].vx}</p></div>
+        <div><p>vy</p><p>{props.objs[selected_obj].vy}</p></div>
+        <div><p>vz</p><p>{props.objs[selected_obj].vz}</p></div>
+        <div><p>ax</p><p>{props.objs[selected_obj].ax}</p></div>
+        <div><p>ay</p><p>{props.objs[selected_obj].ay}</p></div>
+        <div><p>az</p><p>{props.objs[selected_obj].az}</p></div>
+
       </div>
       <div><button>SAVE</button><button>DELETE</button></div>
     </div>
   );
 }
 
-function  Terrain() {
+function  Terrain(props: { objs: any; }) {
+  
+  var   ctx, w: number, h: number;
+
+  var draw_background = (ctx: any) => {
+
+    for (let i = 0; i < w; i += 20) {
+      ctx.beginPath();
+      ctx.moveTo(i, 0);
+      ctx.lineTo(i, h);
+      ctx.stroke();
+    }
+
+    for (let i = 0; i < h; i += 20) {
+      ctx.beginPath();
+      ctx.moveTo(0, i);
+      ctx.lineTo(w, i);
+      ctx.stroke();
+    }
+  }
+
+  var animate = (ctx: any) => {
+    ctx.clearRect(0, 0, w, h);
+    ctx.strokeStyle = "#343434";
+    draw_background(ctx);
+    ctx.strokeStyle = "#ffffff";
+
+    for (let i = 0; i < props.objs.length; i++) {
+      ctx.beginPath();
+      ctx.arc(props.objs[i].x, props.objs[i].y, props.objs[i].radius, 0, 2*Math.PI);
+      ctx.stroke();
+    }
+
+    //requestAnimationFrame(() => animate(ctx))
+  }
+
+  useEffect(() => {
+    var     canvas = document.getElementById("canvas");
+    var     terrain = document.getElementById("terrain");
+
+    ctx = canvas.getContext("2d");
+    w = canvas.width = terrain?.offsetWidth;
+    h = canvas.height = terrain?.offsetHeight;
+    ctx.lineWidth = 1;
+    animate(ctx);
+
+  }, [])
+
   return (
     <div id="terrain">
       <canvas id="canvas">something went wrong!</canvas>
@@ -45,7 +110,18 @@ function  Terrain() {
 export default function Home() {
   var initial_objs = [
     {
-      name: "obj_1"
+      name: "obj_1",
+      mass: 1,
+      radius: 20,
+      x: 50,
+      y: 100,
+      z: 5,
+      vx: 2,
+      vy: 12,
+      vz: 15,
+      ax: 20,
+      ay: 60,
+      az: 3
     }
   ]
 
@@ -61,7 +137,7 @@ export default function Home() {
         </ul>
       </div>
       <div>
-        <Terrain />
+        <Terrain  objs={initial_objs} />
       </div>
     </main>
   );
